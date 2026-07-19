5203e8c10d40e8e8e80e809f1208d9306cc1b4b8
Martin F. Johansen <martinfjohansen@inductive.no>
2026-07-19 12:11:10 +0200

    v0.1 progressing

--- tutorial/x-a-mov-immediates/main.c
+++ tutorial/x-a-mov-immediates/main.c
@@ -8,10 +8,10 @@ int main(){
 
 	test(&ts);
 
-	printf("u8: %x\n", ts.a);
-	printf("u16: %x\n", ts.b);
-	printf("u32: %x\n", ts.c);
-	printf("u64: %lx\n", ts.d);
+	printf("u8: 0x%X\n", ts.a);
+	printf("u16: 0x%X\n", ts.b);
+	printf("u32: 0x%X\n", ts.c);
+	printf("u64: 0x%lX\n", ts.d);
 	
 	printf("s8: %d\n", ts.e);
 	printf("s16: %d\n", ts.f);
@@ -22,7 +22,8 @@ int main(){
 	printf("f64: %.15f\n", ts.j);
 	
 	print_m128_u16(ts.k);
-	//print_m256_u16(ts.l);
+	print_m128_s16(ts.l);
+	//print_m256_u16(ts.m);
 
 	return 0;
 }
